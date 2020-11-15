8bac3de21d1ddf2ca1b628e97fa95d8381611732
redd096 <martin.babino96@gmail.com>
2020-11-15 19:48:09 +0100

    chest fix

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
+    [Header("Important")]
+    [SerializeField] bool isActiveAtStart = false;
+
     [Header("Power Up")]
     [SerializeField] PowerUp powerUp = default;
 
+    [Header("Timer Before Active")]
+    [SerializeField] float timerBeforeActive = 0.5f;
+
     Animator anim;
 
+    float timer;
     bool addPowerUp;
+
+    void OnEnable()
+    {
+        timer = Time.time + timerBeforeActive;
+    }
 
     private void Awake()
     {
+        if (isActiveAtStart == false)
+            gameObject.SetActive(false);
+
         anim = GetComponentInChildren<Animator>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //do only if ended timer
+        if (timer > Time.time)
+            return;
+
         //if hit player
         Player player = collision.gameObject.GetComponentInParent<Player>();
         if(player & addPowerUp == false)
