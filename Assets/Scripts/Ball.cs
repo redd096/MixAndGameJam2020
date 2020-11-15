897842f290da3f10dc3a388be5127b77d9922d54
redd096 <martin.babino96@gmail.com>
2020-11-15 17:11:03 +0100

    fix ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -50,13 +50,15 @@ public class Ball : MonoBehaviour
         Character character = collision.gameObject.GetComponentInParent<Character>();
         if (character)
         {
-            if ((owner is Player && character is Player)
-                || (owner is Enemy && character is Enemy))
-                return;
-
             if (BallThrowed)
             {
                 RemoveBallThrowed();
+
+                //no hit allies
+                if ((owner is Player && character is Player) || (owner is Enemy && character is Enemy))
+                    return;
+
+                //do damage (or deflect or parry)
                 character.HitByBall(this, isParryable);
             }
 
